ba045cd81c07dc45fa578feacedf1d733e95802f
bengris32 <bengris32@protonmail.ch>
2022-09-12 20:17:03 +0100

    selene: Build MediaTek Vibrator HAL

--- device.mk
+++ device.mk
@@ -360,6 +360,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb.gadget@1.1.vendor:64 \
     android.hardware.usb@1.3.vendor:64
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator-service.mediatek
+
 # VNDK
 PRODUCT_PACKAGES += \
     libcrypto-v32 \
