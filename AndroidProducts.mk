a98d94eeae102a562f550af887e98367534063c5
nouboi <atharvnegi1106@gmail.com>
2023-08-13 16:17:57 +0530

    selene: Initial Trees

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The PixelExperience Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_selene.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_selene-user \
+    aosp_selene-userdebug \
+    aosp_selene-eng
