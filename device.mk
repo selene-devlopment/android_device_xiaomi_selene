c90f634bfc834279d89298841cd15f968ff24336
nouboi <atharvnegi1106@gmail.com>
2023-08-14 10:23:28 +0530

    selene: Build oss GPS interfaces

--- device.mk
+++ device.mk
@@ -151,6 +151,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
     android.hardware.gatekeeper@1.0-impl
+	
+# GPS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss.measurement_corrections@1.1.vendor:64 \
+    android.hardware.gnss.visibility_control@1.0.vendor:64 \
+    android.hardware.gnss@2.1.vendor:64
 
 # Health
 PRODUCT_PACKAGES += \
