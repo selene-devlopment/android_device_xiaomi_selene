a2ad98bdbf894f30f16b82e69d4b5008d160ca38
nouboi <atharvnegi1106@gmail.com>
2023-08-14 10:06:24 +0530

    selene: Build oss Health hal

--- device.mk
+++ device.mk
@@ -150,6 +150,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl \
     android.hardware.health@2.1-impl.recovery
 
 # Media
