fdc2e4e4aa802b1abad84481fffe04d05c85358d
NouBoi <atharvnegi1106@gmail.com>
2023-11-05 13:20:21 +0530

    selene: Drop vibrator service

--- device.mk
+++ device.mk
@@ -359,14 +359,6 @@ PRODUCT_PACKAGES += \
     android.hardware.usb.gadget@1.1.vendor:64 \
     android.hardware.usb@1.3.vendor:64
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator-V1-ndk_platform.vendor:64
-	
-PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
-
 # VNDK
 PRODUCT_PACKAGES += \
     libcrypto-v32 \
