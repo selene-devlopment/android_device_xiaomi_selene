3936f6744d0d9d064acfaf0e4fc39c5b16145351
atharvnegi <atharvnegi2951@gmail.com>
2023-12-09 09:47:50 +0530

    selene: Import Initial Overlays
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -233,6 +233,13 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Overlays
+PRODUCT_PACKAGES += \
+    FrameworksResOverlaySelene \
+    SystemUIOverlaySelene
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Properties
 include $(LOCAL_PATH)/vendor_logtag.mk
 
