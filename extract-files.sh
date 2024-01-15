997fc0e505834692800c431b286edef01b044b60
atharvnegi <atharvnegi2951@gmail.com>
2024-01-15 20:30:06 +0530

    [TMP]selene: Drop IMS blobs for now
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -64,9 +64,6 @@ function blob_fixup {
     vendor/lib64/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
         "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
         ;;
-    lib64/libsink.so)
-        "${PATCHELF}" --add-needed "libshim_vtservice.so" "${2}"
-            ;;
     vendor/bin/mtk_agpsd)
         "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v32.so" "${2}"
         ;;
